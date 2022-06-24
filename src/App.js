f4a7c5414ae40070d154f8f6f0cd058a8fd8e0f4
Dinorah Skidelsky <dinorahskidelsky@gmail.com>
2022-06-23 21:09:14 -0300

    finished calculator

--- src/App.js
+++ src/App.js
@@ -1,15 +1,61 @@
 import './App.css';
 import Boton from './componentes/Boton'
+import Pantalla from './componentes/Pantalla'
+import BotonClear from './componentes/BotonClear'
+import { useState } from 'react';
+import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val);
+  };
+
+  const calcularResultado = () => {
+    if (input) {
+      setInput(evaluate(input));
+    } else {
+      alert("please enter a correct value");
+    }
+  };
+
   return (
     <div className="App">
       <div className="title">
-        <h1>Interactive Calculator</h1>
+        <h1>
+          <em>Interactive Calculator</em>
+        </h1>
       </div>
       <div className="contenedor-calculadora">
+        <Pantalla input={input} />
         <div className="fila">
-          <Boton>1</Boton>
+          <Boton manejarClick={agregarInput}>7</Boton>
+          <Boton manejarClick={agregarInput}>8</Boton>
+          <Boton manejarClick={agregarInput}>9</Boton>
+          <Boton manejarClick={agregarInput}>+</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClick={agregarInput}>4</Boton>
+          <Boton manejarClick={agregarInput}>5</Boton>
+          <Boton manejarClick={agregarInput}>6</Boton>
+          <Boton manejarClick={agregarInput}>-</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClick={agregarInput}>1</Boton>
+          <Boton manejarClick={agregarInput}>2</Boton>
+          <Boton manejarClick={agregarInput}>3</Boton>
+          <Boton manejarClick={agregarInput}>*</Boton>
+        </div>
+        <div className="fila">
+          <Boton manejarClick={agregarInput}>0</Boton>
+          <Boton manejarClick={agregarInput}>.</Boton>
+          <Boton manejarClick={calcularResultado}>=</Boton>
+          <Boton manejarClick={agregarInput}>/</Boton>
+        </div>
+        <div className="fila">
+          <BotonClear manejarClear={() => setInput("")}>Clear</BotonClear>
         </div>
         <div className="fila"></div>
         <div className="fila"></div>
--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,12 +1,18 @@
-import react from 'react';
+import React from 'react';
+import '../stylesheet/Boton.css';
 
 function Boton(props) {
+  const esOperador = valor => {
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
+  };
+
   return (
     <div
-      className={`boton-contenedor`}>
+      className={`boton-contenedor ${esOperador(props.children) ? "operador" : ''}`.trimEnd()}
+      onClick={() => props.manejarClick(props.children)}>
       {props.children}
     </div>
   );
-}
+};
 
 export default Boton;
